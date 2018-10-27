13b6cd2a15af63f824d540d669d78bd6ca56f73e
Anoop Varghese <anupvarghese@gmail.com>
2018-10-27 22:20:31 +1100

    tslint prettier and husky

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import express from 'express'
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (_, res) => {
-  res.send('Hellllllloooo');
+app.get("/", (_, res) => {
+  res.send("Hellllllloooo");
 });
 
 app.listen(3000, () => {
+  // tslint:disable-next-line
   console.log(`App started on ${port}`);
 });
