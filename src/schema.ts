c88eacbf05fcdc8c26c75402226c904661f3f090
Anoop Varghese <anupvarghese@gmail.com>
2018-10-28 20:33:09 +1100

    lint

--- src/schema.ts
+++ src/schema.ts
@@ -4,8 +4,8 @@ import {
   GraphQLList,
   GraphQLSchema
 } from "graphql";
-import axios from 'axios';
-import qs from 'querystring';
+import axios from "axios";
+import qs from "querystring";
 
 interface queryArgs {
   ingredients: Array<String>;
@@ -16,12 +16,12 @@ interface queryArgs {
 interface RecipeResponse {
   results: [
     {
-      title: String,
-      href: String,
-      ingredients: Array<String>,
-      thumbnail: String,
+      title: String;
+      href: String;
+      ingredients: Array<String>;
+      thumbnail: String;
     }
-  ]
+  ];
 }
 
 const recipeType = new GraphQLList(
@@ -45,12 +45,16 @@ const queryType = new GraphQLObjectType({
         ingredients: { type: GraphQLList(GraphQLString) },
         dish: { type: GraphQLString }
       },
-      resolve: async (_, { ingredients, dish, page = 1}: queryArgs) => {
-        const query = qs.stringify({ i: ingredients.join(","), q: dish, p: page});
+      resolve: async (_, { ingredients, dish, page = 1 }: queryArgs) => {
+        const query = qs.stringify({
+          i: ingredients.join(","),
+          q: dish,
+          p: page
+        });
         const url = `http://www.recipepuppy.com/api?${query}`;
         return axios.get<RecipeResponse>(url).then(res => {
           return res.data.results;
-        })
+        });
       }
     }
   })
