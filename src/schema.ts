45c601b9a85d9634ae3b4e8df25c6dae6dad26d8
Anoop Varghese <anupvarghese@gmail.com>
2018-10-28 19:44:13 +1100

    another endpoint

--- src/schema.ts
+++ src/schema.ts
@@ -1,9 +1,38 @@
-import { buildSchema } from "graphql";
+import { GraphQLObjectType, GraphQLString, GraphQLList, GraphQLSchema } from "graphql";
 
-const schema = buildSchema(`
-  type Query {
-    hello: String,
-  }
-`);
+interface queryArgs {
+  ingredients: Array<String>
+}
+
+const recipeType = new GraphQLList(new GraphQLObjectType({
+  name: 'recipe',
+  fields: () => ({
+    title: { type: GraphQLString },
+    href: { type: GraphQLString },
+    ingredients: { type: GraphQLString },
+    thumbnail: { type: GraphQLString },
+  }),
+}));
+
+
+const queryType = new GraphQLObjectType({
+  name: "Query",
+  fields: () => ({
+    recipes: {
+      type: recipeType,
+      args: {
+        ingredients: { type: GraphQLList(GraphQLString) },
+      },
+      resolve: (_, { ingredients }: queryArgs ) => [({
+        title: 'Hi',
+        href: 'Hello',
+        ingredients: ingredients.join(","),
+        thumbnail: 'asjk',
+      })]
+    },
+  }),
+});
+
+const schema = new GraphQLSchema({ query: queryType })
 
 export default schema;
